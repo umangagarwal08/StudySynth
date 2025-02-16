4b1e2f266dbf03b6e5893d5dc1f8dbf79c8571e6
Umang Agarwal <59832370+umangagarwal08@users.noreply.github.com>
2025-02-16 21:56:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from rag import rag_pdf, answer_generator
 
 
 st.title("AI-Powered PDF Study Assistant")
-uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"])
+uploaded_file = st.sidebar.file_uploader("Upload a PDF", type=["pdf"])
 
 if uploaded_file is not None:
-    st.success("PDF uploaded successfully!")
+    st.sidebar.success("PDF uploaded successfully!")
     retrival=rag_pdf(uploaded_file)
-    st.write("### Prerequisites Identified:")
+    st.write("### The uploaded file requires some prerequisite knowledge. Please tick the items you are aware of so we can create the modules accordingly.")
 
-    query="""Analyze the provided document and identify the three most essential prerequisite topics required to understand it. List only the topic names in order of importance, without any explanations.
+    Prerequisites="""Analyze the provided document and identify the three most essential prerequisite topics required to understand it. List only the topic names in order of importance, without any explanations.
 
     For example, if the document is about Convolutional Neural Networks (CNNs), the output should be:
 
@@ -20,18 +20,18 @@ if uploaded_file is not None:
     Deep Learning
     Computer Vision
     Ensure that the topics are listed in decreasing order of necessity comma seperated ."""
-    result =answer_generator(retrival,query)
-    items = result.split(",")
+    result =answer_generator(retrival,Prerequisites)
+    Prerequisites = result.split(",")
     #st.markdown(prerequisitory)
 
-    st.title("Knowledge Checklist")
-    st.write("Tick the things that you already know and click submit.")
+    #st.title("Knowledge Checklist")
+    #st.write("Tick the things that you already know and click submit.")
 
     # Dictionary to hold checkbox states
     selected_items = {}
 
     # Create checkboxes for each item
-    for item in items:
+    for item in Prerequisites:
         selected_items[item] = st.checkbox(item)
 
     # Submit button
